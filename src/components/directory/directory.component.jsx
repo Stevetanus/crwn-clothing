dee55189cce6329f3d76e5016d2f65fb8ad91f7d
Steventhibe <steventhibe@gmail.com>
2022-12-24 22:32:40 +0800

    modified categories-preview page and each category page

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,11 +1,11 @@
-import CategoryItem from "../categoryitem/category-item.component";
+import DirectoryItem from "../directoryitem/directory-item.component";
 import "./directory.styles.scss";
 
 const Directory = ({ categories }) => {
   return (
     <div className="directory-container">
       {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
+        <DirectoryItem key={category.id} category={category} />
       ))}
     </div>
   );
---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,35 @@
+import { createContext, useState, useEffect } from "react";
+
+import {
+  addCollectionAndDocuments,
+  getCategoriesAndDocuments,
+} from "../utils/firebase/firebase.utils";
+
+// import SHOP_DATA from "../shop-data";
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+      setCategoriesMap(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+
+  // useEffect(() => {
+  //   addCollectionAndDocuments("categories", SHOP_DATA);
+  // }, []);
+  const value = { categoriesMap };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
