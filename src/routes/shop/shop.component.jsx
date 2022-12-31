98fc0d4069699ad2f5433018edc215358fb21de7
Steventhibe <steventhibe@gmail.com>
2022-12-31 22:46:46 +0800

    remove business logic from firebase/utils to category.selector and import createSelector to prevent duplicate update about same object.

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,7 +5,7 @@ import CategoriesPreview from "../categories-preview/categories-preview.componen
 import Category from "../category/category.component";
 
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategoriesMap } from "../../store/categories/category.action";
+import { setCategories } from "../../store/categories/category.action";
 
 import "./shop.styles.scss";
 
@@ -13,8 +13,8 @@ const Shop = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments("categories");
-      dispatch(setCategoriesMap(categoryMap));
+      const categoriesArray = await getCategoriesAndDocuments("categories");
+      dispatch(setCategories(categoriesArray));
     };
 
     getCategoriesMap();
--- src/store/store.js
+++ src/store/store.js
@@ -7,6 +7,21 @@ import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
+// demystify the middleware(logger)
+// const loggerMiddleware = (store) => (next) => (action) => {
+//   if (!action.type) {
+//     return next(action);
+//   }
+
+//   console.log("type: ", action.type);
+//   console.log("payload: ", action.payload);
+//   console.log("currentState: ", store.getState());
+
+//   next(action);
+
+//   console.log("next state: ", store.getState());
+// };
+
 const middleWares = [logger];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
