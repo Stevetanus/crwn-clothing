e5110eaca36096e3014c14a1619f90f30f14383c
Steventhibe <steventhibe@gmail.com>
2023-02-03 23:23:46 +0800

    finish all categories folder into ts except sagas, and we got state: any for the app working just for now

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -1,6 +1,11 @@
-import { CATEGORIES_ACTION_TYPES, Category } from "./category.types";
+import { Category } from "./category.types";
 
-import { CategoryAction } from "./category.action";
+import {
+  fetchCategoriesStart,
+  fetchCategoriesSuccess,
+  fetchCategoriesFailure,
+} from "./category.action";
+import { AnyAction } from "redux";
 
 export type CategoriesState = {
   readonly categories: Category[];
@@ -16,16 +21,29 @@ export const CATEGORIES_INITIAL_STATE: CategoriesState = {
 
 export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
-  action = {} as CategoryAction
-) => {
-  switch (action.type) {
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
-      return { ...state, isLoading: true };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return { ...state, categories: action.payload, isLoading: false };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return { ...state, error: action.payload, isLoading: false };
-    default:
-      return state;
+  action = {} as AnyAction
+): CategoriesState => {
+  if (fetchCategoriesStart.match(action)) {
+    return { ...state, isLoading: true };
   }
+
+  if (fetchCategoriesSuccess.match(action)) {
+    return { ...state, categories: action.payload, isLoading: false };
+  }
+
+  if (fetchCategoriesFailure.match(action)) {
+    return { ...state, error: action.payload, isLoading: false };
+  }
+  // switch (action.type) {
+  //   case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
+  //     return { ...state, isLoading: true };
+  //   case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+  //     return { ...state, categories: action.payload, isLoading: false };
+  //   case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
+  //     return { ...state, error: action.payload, isLoading: false };
+  //   default:
+  //     return state;
+  // }
+
+  return state;
 };
